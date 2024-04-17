1c4463a49eee582753dff28c6c65f784c4f06860
gabrielmuller30 <gabrielmuller30.gml@gmail.com>
2024-04-17 15:50:19 -0300

    add code. obs: prompt is not function

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-let nome = prompt ("Digite seu nome: ");
-let xp = prompt ("Insira seus pontos de experiência (XP): ");
+let nome = "GaMullers";
+let xp = 6533;
+let nivel;
+
+//while (!nome){
+//    nome = prompt("Digite seu nome: ");
+//}
+
+//    if (!nome) {
+//        console.log("Você não digitou nenhum nome. Por favor, insira seu nome.");
+//    }
 
 
-switch (xp = true){
-    case xp < 1000:
-        xp = "Ferro";
-        break;
-    case xp >= 1001 && xp <= 2000:
-        xp = "Bronze";
-        break;
-    case xp >= 2001 && xp <= 5000:
-        xp = "Prata";
-        break;
-    case xp >= 5001 && xp <= 7000:
-        xp = "Ouro";
-        break;
-    case xp >= 7001 && xp <= 8000:
-        xp = "Platina";
-        break;
-    case xp >= 8001 && xp <= 9000:
-        xp = "Ascendente";
-        break;
-    case xp >= 9001 && xp <= 10000:
-        xp = "Imortal";
-        break;
-    case xp >= 10001:
-        xp = "Radiante";
-        break;
+if (xp < 1000){
+    nivel = "Ferro";
+}else if (xp >= 1001 && xp <= 2000){
+    nivel = "Bronze";
+}else if (xp >= 2001 && xp <= 5000){
+    nivel = "Prata";
+}else if (xp >= 5001 && xp <= 7000){
+    nivel = "Ouro";
+}else if (xp >= 7001 && xp <= 8000){
+    nivel = "Platina";
+}else if (xp >= 8001 && xp <= 9000){
+    nivel = "Ascendente";
+}else if (xp >= 9001 && xp <= 10000){
+    nivel = "Imortal";
+}else if (xp >= 10001){
+    nivel = "radiante"; 
 }
 
-console.log("O herói de nome " + nome + " está no nível de " + xp)
+console.log("O herói de nome " + nome + " está no nível " + nivel);
